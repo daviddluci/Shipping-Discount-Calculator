8980c3010c8f4c8ead28fb2336cbe455639fa6bc
daviddluci <gliaubertasdeividas@gmail.com>
2025-07-20 16:40:02 +0300

    refactored to satisfy rubocop

--- lib/discount_rules.rb
+++ lib/discount_rules.rb
@@ -15,31 +15,53 @@ module DiscountRules
   end
 
   def self.calculate_discount(date, size, carrier)
-    ym = date.strftime('%Y-%m')
+    year_month = date.strftime('%Y-%m')
     base_price = PRICES[carrier][size]
+    discount, reduced_price = size_based_discount(year_month, size, carrier, base_price)
+    discount, reduced_price = apply_monthly_discount_limit(year_month, discount, reduced_price, base_price)
+
+    @monthly_discounts[year_month] += discount
+    [reduced_price.round(2), discount.round(2)]
+  end
+
+  def self.size_based_discount(year_month, size, carrier, base_price)
+    case size
+    when 'S' then small_size_discount(base_price)
+    when 'L' then large_size_discount(year_month, carrier, base_price)
+    else [0.0, base_price]
+    end
+  end
+
+  def self.small_size_discount(base_price)
+    lowest_s_price = PRICES.values.map { |p| p['S'] }.min
+    discount = base_price - lowest_s_price
+    reduced_price = lowest_s_price
+    [discount, reduced_price]
+  end
+
+  def self.large_size_discount(year_month, carrier, base_price)
     discount = 0.0
     reduced_price = base_price
 
-    if size == 'S'
-      lowest_s_price = PRICES.values.map { |p| p['S'] }.min
-      discount = base_price - lowest_s_price
-      reduced_price = lowest_s_price
-
-    elsif size == 'L' && carrier == 'LP'
-      @lp_large_count[ym] += 1
-      if @lp_large_count[ym] == 3
+    if carrier == 'LP'
+      @lp_large_count[year_month] += 1
+      if @lp_large_count[year_month] == 3
         discount = base_price
         reduced_price = 0.0
       end
     end
 
-    total_discount_so_far = @monthly_discounts[ym]
+    [discount, reduced_price]
+  end
+
+  def self.apply_monthly_discount_limit(year_month, discount, reduced_price, base_price)
+    total_discount_so_far = @monthly_discounts[year_month]
+
     if total_discount_so_far + discount > 10.0
       discount = [0.0, 10.0 - total_discount_so_far].max
       reduced_price = base_price - discount
     end
 
-    @monthly_discounts[ym] += discount
-    [reduced_price.round(2), discount.round(2)]
+    [discount, reduced_price]
   end
 end
